5ee6c4e0be8e3300597f0117e38c4adcc69525ce
Jhonathan Cordeiro <jhonathan.cordeiro@ebix.com>
2024-09-13 15:07:28 -0300

    Input do usuario

---
+++ udemy-geek_university-python/recebendo_dados_usuario.py
@@ -0,0 +1,12 @@
+#Entrada de dados
+nome = input('Qual é o seu nome?')
+
+print(f'Seja bem vindo(a) {nome}')
+
+idade = int(input('Qual a sua idade?'))
+
+#Processa
+
+#Saida
+print(f'{nome} tem {idade} anos')
+print(f'{nome} nasceu em {2024-idade}')
