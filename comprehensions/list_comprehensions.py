eff0b8f0e4ce79e69d8ed52ec289bad81adad58b
Jhonathan Cordeiro <jhonathan.cordeiro@ebix.com>
2024-10-02 22:00:53 -0300

    Feat(): Finalizado aula sobre comprehensions

--- comprehensions/list_comprehensions.py
+++ comprehensions/list_comprehensions.py
@@ -8,7 +8,6 @@ numeros: list[int] = [1, 2, 3, 4, 5]
 
 resultado = [numero*2 for numero in numeros]
 print(f'Resultado: {resultado}')
-
 # Exemplo 2
 lista_numeros = [2, 3, 5, 7, 9, 11, 44, 6, 77, 9, 0]
 numeros_pares = [numero for numero in lista_numeros if numero % 2 == 0]
---
+++ comprehensions/set_comprehensions.py
@@ -0,0 +1,10 @@
+"""
+Set Comprehension
+
+lista = [1, 2, 3, 4, 5]
+set = {1, 2, 3, 4, 5}
+"""
+
+# Exemplos
+set_numeros = {num for num in range(1, 10)}
+print(set_numeros)
