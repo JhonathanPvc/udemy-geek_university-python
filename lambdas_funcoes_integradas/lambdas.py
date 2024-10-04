e5a26c2c698a7b4369e1c120ead42bdf83667a40
Jhonathan Cordeiro <jhonathan.cordeiro@ebix.com>
2024-10-03 21:38:11 -0300

    Feat(): Modulos dentro do projeto

---
+++ lambdas_funcoes_integradas/lambdas.py
@@ -0,0 +1,16 @@
+"""
+Utilizando Lambdas
+  Sao funcoes sem nomes, ou seja, anonimas
+"""
+
+# Funcoes
+
+
+def retorna_quadrado(x):
+    return x**2
+
+
+print(f'O quadrado de 3 eh: {retorna_quadrado(3)}')
+
+# Lambda
+lambda x: x**2
---
+++ modulos/random_aula.py
@@ -0,0 +1,12 @@
+"""
+  Modulo Random
+"""
+
+# Forma de import 1 (Nao recomendado)
+# import random
+
+# Forma de import 2
+from random import random
+
+for i in range(10):
+    print(random())
